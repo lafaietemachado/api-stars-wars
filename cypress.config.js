26c19e4bdec5fdb40f907cbf2231565e28111cf6
Lafaiete Rodrigues Machado <lafaiete.machado@gmail.com>
2023-09-20 19:00:45 -0300

    adicionado Mochawesome para geração de relatorios

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,13 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'https://swapi.dev/api/',
     setupNodeEvents(on, config) {
+      { "reporter"; "mochawesome",
+        "reporterOptions";
+          { "reportDir"; "cypress/report/mochawesome-report",
+          "overwrite"; true,
+          "html"; true,
+          "json"; false,
+          "timestamp"; "mmddyyyy_HHMMss" }}
       // implement node event listeners here
     },
   },
